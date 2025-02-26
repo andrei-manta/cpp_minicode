0f62c714adc17f83882d30ff42e6acd552d468b8
andrei-manta <andreimanta100@gmail.com>
2025-02-27 00:19:39 +0200

    C++ constexpr usage with pointers and references

---
+++ constexpr_ptr_and_ref.cpp
@@ -0,0 +1,25 @@
+//a and b are global-scope, hence their address is fixed
+int a = 5;
+const int b = 7;
+
+//1. a is a fixed-address variable (necessary for constexpr ptr and ref),
+//2. p1's low-level is compatible with a's top-level(nonconst and nonconst), hence
+//constexpr is - LEGAL
+constexpr int *p1 = &a;
+//const int initialises nonconst int * - ILLEGAL
+//constexpr int *p2 = &b;
+//nonconst int converted to const int *, hence constexpr is - LEGAL
+constexpr const int *p3 = &a;
+//const qualifier match - LEGAL
+constexpr const int *p4 = &b;
+
+//exact same as above
+constexpr int &r1 = a;
+//constexpr int &r2 = b;
+constexpr const int &r3 = a;
+constexpr const int &r4 = b;
+
+int main()
+{
+        return 0;
+}
