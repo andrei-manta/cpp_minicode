66ff8792e15f78ec4587326697f37e31e6328358
andrei-manta <andreimanta100@gmail.com>
2025-02-28 13:21:18 +0200

    Update constexpr_ptr_and_ref.cpp

--- constexpr_ptr_and_ref.cpp
+++ constexpr_ptr_and_ref.cpp
@@ -1,10 +1,12 @@
+//constexpr forces TOP-LEVEL const
+
 //a and b are global-scope, hence their address is fixed
 int a = 5;
 const int b = 7;
 
 //1. a is a fixed-address variable (necessary for constexpr ptr and ref),
 //2. p1's low-level is compatible with a's top-level(nonconst and nonconst), hence
-//constexpr is - LEGAL
+//constexpr is - LEGAL; p1's top-level is CONST, from the constexpr
 constexpr int *p1 = &a;
 //const int initialises nonconst int * - ILLEGAL
 //constexpr int *p2 = &b;
@@ -18,6 +20,10 @@ constexpr int &r1 = a;
 //constexpr int &r2 = b;
 constexpr const int &r3 = a;
 constexpr const int &r4 = b;
+
+//constexpr pointers can also be initialised with nullptr and 0
+constexpr int *p5 = 0;
+constexpr int *p6 = nullptr;
 
 int main()
 {
