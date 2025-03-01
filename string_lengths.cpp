ddba12688cdc4a2f763be0cc0d0d2105b3f2b954
andrei-manta <andreimanta100@gmail.com>
2025-03-01 12:19:19 +0200

    C++ demo of various types of strings' lengths

---
+++ string_lengths.cpp
@@ -0,0 +1,13 @@
+#include <string>
+
+int main()
+{
+        //the length of an emty string is 0
+        std::string s1;
+
+        //the length of a STRING LITERAL is number of chars + NULL TERMINATOR,
+        //so "" has length 1, but s2 has length 0
+        std::string s2 = "";
+
+        return 0;
+}
